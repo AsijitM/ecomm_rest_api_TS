bceea8ffe145eb7594cea0000003c960f4fe1eed
AsijitM <asijit00@gmail.com>
2024-02-07 10:11:15 +0530

    first commit

---
+++ src/routes/auth.ts
@@ -0,0 +1,10 @@
+import { login, signup } from '../controllers/auth';
+
+import { Router } from 'express';
+
+const authRoutes: Router = Router();
+
+authRoutes.post('/signup', signup);
+authRoutes.post('/login', login);
+
+export default authRoutes;
