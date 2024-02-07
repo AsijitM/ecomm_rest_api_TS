53135cfba5b055f98d9745c12c0d94fe0fc6c3fa
AsijitM <asijit00@gmail.com>
2024-02-07 10:20:33 +0530

    error-codes

---
+++ src/exceptions/root.ts
@@ -0,0 +1,27 @@
+//message, status Code, error codes, error
+
+export class HttpException extends Error {
+  message: string;
+  errorCode: any;
+  statusCode: number;
+  errors: ErrorCode;
+
+  constructor(
+    message: string,
+    errorCode: ErrorCode,
+    statusCode: number,
+    error: any
+  ) {
+    super(message);
+    this.errorCode = errorCode;
+    this.message = message;
+    this.statusCode = statusCode;
+    this.errors = error;
+  }
+}
+
+export enum ErrorCode {
+  USER_NOT_FOUND = 1001,
+  USER_ALREADY_EXSIST = 1002,
+  INCORRECT_PASSWORD = 1003,
+}
